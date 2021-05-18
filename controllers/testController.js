d161c285deb5e0001ddb08347335408a07820b49
rafi nizar <rafi.nizar15@gmail.com>
2021-05-18 17:43:19 +0700

    added: test file controllers
    uploading and fetching files to firebase storage bucket

--- controllers/testController.js
+++ controllers/testController.js
@@ -1,5 +1,12 @@
-const { db } = require('@util/firebase')
+const { bucket, db } = require('@util/firebase')
 const usersCol = db.collection('users')
+
+const saltedMd5 = require('salted-md5')
+const path = require('path')
+
+const express = require('express')
+const { storage } = require('firebase-admin')
+const app = express()
 
 const index_test = async (req, res) => {
 
@@ -11,6 +18,12 @@ const index_test = async (req, res) => {
         await docRef.set({
             age: 21,
             email: 'rafi@email.com'
+        })
+
+        // cara lain pake add
+        await usersCol.add({
+            age: 21,
+            email: 'rafi2@email.com'
         })
     
         res.status(200).send({
@@ -24,6 +37,46 @@ const index_test = async (req, res) => {
     
 }
 
+const file_test = async (req, res) => {
+
+    // console.log(req)
+
+    const name = saltedMd5(req.file.originalname, process.env.FILENAME_SALT)
+    const filename = name + path.extname(req.file.originalname)
+    
+    await bucket.file('user_images/' + filename).createWriteStream()
+        .on('error', (err) => res.status(500).send({ message: "Internal server error. "}))
+        .end(req.file.buffer)
+        .on('response', (storageResponse) => {
+            console.log(storageResponse)
+            res.status(200).send({
+                filename,
+                message: 'Upload successfull.'
+            })
+        })
+        
+}
+
+const file_download = async (req, res) => {
+
+    await bucket.file('user_images/5d8544ba44f96a7735ccc71001341de0.PNG').createReadStream()
+        .on('error', (err) => res.status(500).send({ message: "Internal server error. "}))
+        .on('response', (storageResponse) => {
+            // console.log(storageResponse)
+            res.setHeader(
+                "content-type", storageResponse.headers["content-type"]
+            )
+            res.setHeader(
+                "content-length", storageResponse.headers["content-length"]
+            )
+            res.status(200)
+        })
+        .on('end', () => res.end())
+        .pipe(res)
+}
+
 module.exports = {
-    index_test
+    index_test,
+    file_test,
+    file_download
 }
