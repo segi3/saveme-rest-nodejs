70c284ebe32261339a752af5e3bd00a138467372
rafi nizar <rafi.nizar15@gmail.com>
2021-05-21 17:59:56 +0700

    added: testRoute for hash

--- routes/testRoute.js
+++ routes/testRoute.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const testController = require('@controller/testController')
 
+const saltedMd5 = require('salted-md5')
+
 const multer = require('multer')
 const upload = multer({ storage: multer.memoryStorage() })
 
@@ -11,5 +13,12 @@ router.post('/p', testController.index_test)
 router.post('/file', upload.single('file'), testController.file_test)
 router.get('/dl', testController.file_download)
 router.post('/fcm', testController.fcm_test)
+
+router.post('/hash', (req, res) => {
+    res.send({
+        string: req.body.string,
+        hash: saltedMd5(req.body.string, 'mudahkan kami menyelesaikan project ini')
+    })
+})
 
 module.exports = router
