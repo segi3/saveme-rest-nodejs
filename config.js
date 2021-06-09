48b0f73fa85ee23616843c5e4fd769548405f901
rafi nizar <rafi.nizar15@gmail.com>
2021-06-09 19:08:28 +0700

    uneeded bit of codes but sort of needed
    file that is required in the old code but is not used anymore in the current version, will clean up later

---
+++ config.js
@@ -0,0 +1,7 @@
+// config.js
+// deprecated, still pushed because have not clean up
+module.exports = {
+    'secret': 'B21-CAP0083-SUKSES-TOP',
+    'token-secret': 'B21-CAP0083-SUKSES-TOP',
+    'refresh-token-secret': 'b21-cap0083-sukses-top'
+};
